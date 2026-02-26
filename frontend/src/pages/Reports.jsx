095043179808b9b1132a608389aa129f98233ece
anmolgupta2704 <2k23.cscys2312044@gmail.com>
2026-02-26 21:04:01 +0530

    Update Reports.jsx

--- frontend/src/pages/Reports.jsx
+++ frontend/src/pages/Reports.jsx
@@ -3,6 +3,7 @@ export default function Reports() {
     <div className="min-h-screen bg-zinc-900 text-white p-10">
       <h1 className="text-2xl font-bold">Reports</h1>
       <p>All scan history will appear here.</p>
+      <p>Done</p>
     </div>
   );
 }
