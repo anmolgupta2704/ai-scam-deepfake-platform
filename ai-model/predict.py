6604afc16bb2f380c38d488369ee3c29d15c2916
anmolgupta2704 <2k23.cscys2312044@gmail.com>
2026-03-08 14:32:11 +0530

    updated train_model.py

--- ai-model/predict.py
+++ ai-model/predict.py
@@ -10,22 +10,24 @@ file_path = sys.argv[1]
 
 img = cv2.imread(file_path)
 img = cv2.resize(img,(128,128))
+img = img / 255.0
+
 img = np.expand_dims(img,axis=0)
 
 prediction = model.predict(img)[0][0]
 
 if prediction > 0.5:
 
-result = {
-"label":"Deepfake",
-"confidence": float(prediction)
-}
+    result = {
+        "label":"Deepfake",
+        "confidence": float(prediction)
+    }
 
 else:
 
-result = {
-"label":"Real",
-"confidence": float(1-prediction)
-}
+    result = {
+        "label":"Real",
+        "confidence": float(1-prediction)
+    }
 
 print(json.dumps(result))
--- ai-model/train_model.py
+++ ai-model/train_model.py
@@ -1,9 +1,32 @@
 import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
-model = Sequential([
+datagen = ImageDataGenerator(rescale=1./255)
 
+train_data = datagen.flow_from_directory(
+"dataset/train",
+target_size=(128,128),
+batch_size=32,
+class_mode="binary"
+)
+
+valid_data = datagen.flow_from_directory(
+"dataset/valid",
+target_size=(128,128),
+batch_size=32,
+class_mode="binary"
+)
+
+test_data = datagen.flow_from_directory(
+"dataset/test",
+target_size=(128,128),
+batch_size=32,
+class_mode="binary"
+)
+
+model = Sequential([
 layers.Conv2D(32,(3,3),activation="relu",input_shape=(128,128,3)),
 layers.MaxPooling2D(),
 
@@ -14,17 +37,22 @@ layers.Flatten(),
 
 layers.Dense(128,activation="relu"),
 layers.Dense(1,activation="sigmoid")
-
 ])
 
 model.compile(
-
 optimizer="adam",
 loss="binary_crossentropy",
 metrics=["accuracy"]
-
 )
 
-model.fit(train_data,epochs=10)
+model.fit(
+train_data,
+epochs=5,
+validation_data=valid_data
+)
+
+model.evaluate(test_data)
 
 model.save("deepfake_model.h5")
+
+print("Model training completed")
